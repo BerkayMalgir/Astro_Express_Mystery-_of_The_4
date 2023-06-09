c1a5c17f1ed5f64518d4e7f29d6124afa69fa5af
Kahraman <kahraman.erdal.uygur@outlook.com>
2023-06-09 12:45:40 +0300

    Demo finished.

--- Assets/Script_Kahraman/MainMenu.cs
+++ Assets/Script_Kahraman/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour
     public GameObject settingsMenu;
     public GameObject pauseMenu;
     private bool _isPlay;
+    public GameObject cutScene1;
+    public GameObject cutScene2;
+    public GameObject cutScene3;
+    public GameObject cutScene4;
+    public GameObject cutScene5;
+    public GameObject cutScene6;
+    public GameObject cutScene7;
+    public GameObject cutScene8;
+    public GameObject cutScene9;
+
     public void startGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -43,5 +53,53 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
-
+    public void nextScene()
+    {
+        mainMenu.SetActive(false);
+        cutScene1.SetActive(true);
+    }
+    public void nextScene2()
+    {
+        cutScene1.SetActive(false);
+        cutScene2.SetActive(true);
+    }
+    public void nextScene3()
+    {
+        cutScene2.SetActive(false);
+        cutScene3.SetActive(true);
+    }
+    public void nextScene4()
+    {
+        cutScene3.SetActive(false);
+        cutScene4.SetActive(true);
+    }
+    public void nextScene5()
+    {
+        cutScene4.SetActive(false);
+        cutScene5.SetActive(true);
+    }
+    public void nextScene6()
+    {
+        cutScene5.SetActive(false);
+        cutScene6.SetActive(true);
+    }
+    public void nextScene7()
+    {
+        cutScene6.SetActive(false);
+        cutScene7.SetActive(true);
+    }
+    public void nextScene8()
+    {
+        cutScene7.SetActive(false);
+        cutScene8.SetActive(true);
+    }
+    public void nextScene9()
+    {
+        cutScene8.SetActive(false);
+        cutScene9.SetActive(true);
+    }
+    public void lastScene()
+    {
+        cutScene9.SetActive(false);
+    }
 }
